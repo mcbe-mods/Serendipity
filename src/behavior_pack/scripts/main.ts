481e4e1b404a33a14a13c70db620a1f1227a2da1
Lete114 <me@imlete.cn>
2023-10-15 16:41:32 +0800

    feat: support 1.20.3x version

--- src/behavior_pack/scripts/main.ts
+++ src/behavior_pack/scripts/main.ts
@@ -1,47 +1,52 @@
 import {
   world,
   system,
+  Player,
   ItemStack,
-  MinecraftItemTypes,
-  MinecraftEntityTypes,
-  DynamicPropertiesDefinition
+  DynamicPropertiesDefinition,
+  PlayerBreakBlockAfterEventSignal
 } from '@minecraft/server'
+import { MinecraftItemTypes } from '@minecraft/vanilla-data'
 import { getRandomProbability } from '@mcbe-mods/utils'
-import type { ItemType } from '@minecraft/server'
 
-type MinecraftItemTypesKeys = keyof typeof MinecraftItemTypes
+/* ---------- Versions Adaptation ---------- */
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const afterEvents = ((world.afterEvents as any).playerBreakBlock ||
+  (world.afterEvents as any).blockBreak) as PlayerBreakBlockAfterEventSignal
+/* eslint-enable @typescript-eslint/no-explicit-any */
+
+/* ---------- Versions Adaptation ---------- */
+
+const itemTypes = Object.values(MinecraftItemTypes)
 const PROB = 'prob'
 
 world.afterEvents.worldInitialize.subscribe((e) => {
   const def = new DynamicPropertiesDefinition()
   def.defineNumber(PROB, 1)
-  e.propertyRegistry.registerEntityTypeDynamicProperties(def, MinecraftEntityTypes.player)
+  e.propertyRegistry.registerWorldDynamicProperties(def)
 })
 
 system.afterEvents.scriptEventReceive.subscribe((e) => {
   const { id, message } = e
-  if (id === 'serendipity:prob') {
-    const prob = +message
-    if (isNaN(prob)) return
-    world.getAllPlayers().forEach((player) => {
-      player.setDynamicProperty(PROB, prob)
-    })
+  const prob = +message
+  if (isNaN(prob)) return
+
+  const player = e.sourceEntity as Player
+  if (id === 'serendipity:prob' && player?.isOp()) {
+    world.setDynamicProperty(PROB, prob)
   }
 })
 
-world.afterEvents.blockBreak.subscribe(async (e) => {
+afterEvents.subscribe(async (e) => {
   const { dimension, block, player } = e
-  const prob = +(player.getDynamicProperty(PROB) || 1)
-  if (isNaN(prob)) return
+  const prob = (world.getDynamicProperty(PROB) as number) || 1
   const is = getRandomProbability(prob)
   if (!is) return
 
   player.playSound('random.orb', { pitch: 0.5, volume: 0.5 })
-  const itemTypes = Object.keys(MinecraftItemTypes) as MinecraftItemTypesKeys[]
   const randomIndex = Math.floor(Math.random() * itemTypes.length)
-  const randomKey = itemTypes[randomIndex]
+  const randomItemName = itemTypes[randomIndex]
 
-  const item = MinecraftItemTypes[randomKey] as ItemType
-  dimension.spawnItem(new ItemStack(item), block.location)
+  dimension.spawnItem(new ItemStack(randomItemName), block.location)
 })
