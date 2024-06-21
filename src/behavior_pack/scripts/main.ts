f69f9d51dc6a9a65d27427cf64f0f543a6234de6
Lete114 <me@imlete.cn>
2024-06-21 18:52:28 +0800

    feat: support 1.21.x stable

--- src/behavior_pack/scripts/main.ts
+++ src/behavior_pack/scripts/main.ts
@@ -1,29 +1,13 @@
-import { world, system, Player, ItemStack, ItemTypes } from '@minecraft/server'
+import { world, ItemStack } from '@minecraft/server'
+import { MinecraftItemTypes } from '@minecraft/vanilla-data'
 import { getRandomProbability } from '@mcbe-mods/utils'
 
-const itemTypes = ItemTypes.getAll().map((item) => item.id)
-const PROB = 'prob'
-
-world.afterEvents.worldInitialize.subscribe(() => {
-  if (world.getDynamicProperty(PROB)) return
-  world.setDynamicProperty(PROB, 1)
-})
-
-system.afterEvents.scriptEventReceive.subscribe((e) => {
-  const { id, message } = e
-  const prob = +message
-  if (isNaN(prob)) return
-
-  const player = e.sourceEntity as Player
-  if (id === 'serendipity:prob' && player?.isOp()) {
-    world.setDynamicProperty(PROB, prob)
-  }
-})
+const itemTypes = Object.values(MinecraftItemTypes)
+const PROBABILITY = 1
 
 world.afterEvents.playerBreakBlock.subscribe(async (e) => {
   const { dimension, block, player } = e
-  const prob = (world.getDynamicProperty(PROB) as number) || 1
-  const is = getRandomProbability(prob)
+  const is = getRandomProbability(PROBABILITY)
   if (!is) return
 
   player.playSound('random.orb', { pitch: 0.5, volume: 0.5 })
